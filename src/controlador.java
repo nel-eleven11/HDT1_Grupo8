a846850b9542bae94205ea05fd23152e6b8edf07
Japo <pue22296@uvg.edu.gt>
2023-01-15 17:28:54 -0600

    Controlador del programa agregado y pruebas unitarias realizadas

---
+++ src/controlador.java
@@ -0,0 +1,44 @@
+package src;
+
+public class controlador {
+
+    public IRadio radio = new radio();
+
+    public void turnOnOff() {
+        if(radio.isOn()){
+            radio.off();
+        }else{
+            radio.on();
+        }
+    }
+
+    public void changeFrequence() throws Exception {
+        radio.setFrequence(radio.getFrequence());
+    }
+
+    public void forward(){
+        radio.Forward();
+    }
+
+    public void backward(){
+        radio.Backward();
+    }
+
+    public void saveStation(int slot){
+        if(radio.getFrequence().equals("AM")){
+            radio.saveAMStation(radio.getAMActualStation(), slot);
+        }else{
+            radio.saveFMStation(radio.getFMActualStation(), slot);
+        }
+    }
+
+    public void selectFavoriteSation(int slot){
+        if(radio.getFrequence().equals("AM")){
+            radio.setAMActualStation(radio.getAMSlot(slot));
+        }else{
+            radio.setFMActualStation(radio.getFMSlot(slot));
+        }
+
+    }
+
+}
---
+++ src/radio.java
@@ -0,0 +1,114 @@
+package src;
+
+
+public class radio implements IRadio {
+
+    private boolean on;
+    private String frequence;
+    private double FMActualStation;
+    private int AMActualStation;
+    private double[] FMSlot = new double[12];
+    private int[] AMSlot = new int[12];
+
+    public radio(){
+        on = false;
+        frequence = "AM";
+        FMActualStation = 87.9;
+        AMActualStation = 530;
+    }
+
+    public void on(){
+        on = true;
+    }
+
+    public void off(){
+        on = false;
+    }
+
+    public boolean isOn(){
+        return on;
+    }
+
+    public void setFrequence(String freq) throws Exception{
+        try {
+            if(freq.equals("AM") ){
+                frequence = "FM";
+            }else if(freq.equals("FM")){
+                frequence = "AM";
+            }else{
+                throw new Exception("Error");
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+
+    }
+
+    public String getFrequence(){
+        return frequence;
+    }
+
+    public void Forward(){
+        if(frequence.equals("AM")){
+            if(AMActualStation == 1610){
+                AMActualStation = 530;
+            }else{
+                AMActualStation += 10;
+            }
+        }else{
+            if(FMActualStation == 107.9){
+                FMActualStation = 87.9;
+            }else{
+                FMActualStation = Math.round((FMActualStation + 0.2) * 10.0) / 10.0;
+            }
+        }
+    }
+
+    public void Backward(){
+        if(frequence.equals("AM")){
+            if(AMActualStation == 530){
+                AMActualStation = 1610;
+            }else{
+                AMActualStation -= 10;
+            }
+        }else{
+            if(FMActualStation == 87.9){
+                FMActualStation = 107.9;
+            }else{
+                FMActualStation = Math.round((FMActualStation - 0.2) * 10.0) / 10.0;
+            }
+        }
+    }
+
+    public double getFMActualStation(){
+        return FMActualStation;
+    }
+
+    public int getAMActualStation(){
+        return AMActualStation;
+    }
+
+    public void setFMActualStation(double actualStation){
+        FMActualStation = actualStation;
+    }
+
+    public void setAMActualStation(int actualStation){
+        AMActualStation = actualStation;
+    }
+
+    public void saveFMStation(double actualStation, int slot){
+        FMSlot[slot] = actualStation;
+    }
+
+    public void saveAMStation(int actualStation, int slot){
+        AMSlot[slot] = actualStation;
+    }
+
+    public double getFMSlot(int slot){
+        return FMSlot[slot];
+    }
+
+    public int getAMSlot(int slot){
+        return AMSlot[slot];
+    }
+}
