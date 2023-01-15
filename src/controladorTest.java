314d5cebb66b15eda9b0eb93f17d5930ad537a5d
Japo <pue22296@uvg.edu.gt>
2023-01-15 17:26:37 -0600

    Controlador del programa agregado y pruebas unitarias realizadas

---
+++ src/controladorTest.java
@@ -0,0 +1,107 @@
+package src;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class controladorTest {
+
+    @Test
+    void turnOnOff() {
+
+        controlador controlador = new controlador();
+        controlador.turnOnOff();
+        assertTrue(controlador.radio.isOn());
+        controlador.turnOnOff();
+        assertFalse(controlador.radio.isOn());
+    }
+
+    @Test
+    void changeFrequence() {
+        controlador controlador = new controlador();
+        try {
+            controlador.changeFrequence();
+            assertEquals("FM", controlador.radio.getFrequence());
+            controlador.changeFrequence();
+            assertEquals("AM", controlador.radio.getFrequence());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void forward() throws Exception {
+        controlador controlador = new controlador();
+        controlador.forward();
+        assertEquals(540, controlador.radio.getAMActualStation());
+        controlador.forward();
+        assertEquals(550, controlador.radio.getAMActualStation());
+
+        controlador.changeFrequence();
+        controlador.forward();
+        assertEquals(88.1, controlador.radio.getFMActualStation());
+        controlador.forward();
+        assertEquals(88.3, controlador.radio.getFMActualStation());
+
+    }
+
+    @Test
+    void backward() throws Exception {
+        controlador controlador = new controlador();
+        controlador.backward();
+        assertEquals(1610, controlador.radio.getAMActualStation());
+        controlador.backward();
+        assertEquals(1600, controlador.radio.getAMActualStation());
+
+        controlador.changeFrequence();
+        controlador.backward();
+        assertEquals(107.9, controlador.radio.getFMActualStation());
+        controlador.backward();
+        assertEquals(107.7, controlador.radio.getFMActualStation());
+
+
+    }
+
+    @Test
+    void saveStation() throws Exception {
+        controlador controlador = new controlador();
+        controlador.saveStation(1);
+        assertEquals(530, controlador.radio.getAMSlot(1));
+
+        controlador.forward();
+        controlador.forward();
+        controlador.saveStation(2);
+        assertEquals(550, controlador.radio.getAMSlot(2));
+
+        controlador.changeFrequence();
+        controlador.saveStation(1);
+        assertEquals(87.9, controlador.radio.getFMSlot(1));
+
+        controlador.forward();
+        controlador.forward();
+        controlador.saveStation(2);
+        assertEquals(88.3, controlador.radio.getFMSlot(2));
+    }
+
+    @Test
+    void selectFavoriteSation() throws Exception {
+        controlador controlador = new controlador();
+        controlador.saveStation(1);
+
+        controlador.forward();
+        controlador.forward();
+        controlador.saveStation(2);
+
+        controlador.forward();
+        controlador.forward();
+
+        assertEquals(570, controlador.radio.getAMActualStation());
+
+        controlador.selectFavoriteSation(1);
+        assertEquals(530, controlador.radio.getAMActualStation());
+
+        controlador.selectFavoriteSation(2);
+        assertEquals(550, controlador.radio.getAMActualStation());
+
+    }
+}
