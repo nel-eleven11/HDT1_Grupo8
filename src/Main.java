715e04b827175a3f18dcceff46c96378e2870083
Japo <pue22296@uvg.edu.gt>
2023-01-17 22:34:08 -0600

    Correcciones realizadas al main

--- src/Main.java
+++ src/Main.java
@@ -8,85 +8,81 @@ public class Main{
         Scanner teclado = new Scanner(System.in);
         String menuInicial = "1. Encender Radio\n2. salir";
         String menu = "1.Cambia de AM a FM \n2.Avanzar en el dial de las emisoras. Al llegar al final del dial inicia nuevamente. \n3.Permite guardar una emisora en uno de los 12 botones \n4.Permite seleccionar la emisora puesta en un botón \n5. Apagar el radio\n";
+
+
         int opcion = 1;
-        System.out.println(menuInicial);
-        System.out.println("Ingrese la opción que desea: ");
-        opcion = teclado.nextInt();
-        teclado.nextLine();
-        while (opcion<2 && opcion>=1){
+        while (opcion != 2){
+            System.out.println(menuInicial);
+            System.out.println("Ingrese la opción que desea: ");
+            opcion = Integer.parseInt(teclado.nextLine());
             switch (opcion){
-                case 1: {
-                radioUvg.turnOnOff();
-                System.out.println("Radio encendida");  
-                int op = 1;
-                System.out.println(menu);
-                System.out.println("Ingrese la opción que desea: ");
-                op = teclado.nextInt();
-                teclado.nextLine();
-                while (op<5 && op>=1){
-                    switch (op){ 
-                        case 1:{
-                            radioUvg.changeFrequence();
-                            System.out.println(radioUvg.getFrequence());
-                            System.out.println(menu);
-                            System.out.println("Ingrese la opción que desea: ");
-                            opcion = teclado.nextInt();
-                            teclado.nextLine();
-                            break;
-                        }
-                        case 2:{
-                            System.out.println("presione 1 para avanzar y presione 2 para retroceder");
-                            System.out.println("usted esta en la frecuencia:");  
-                            System.out.println(radioUvg.getFrequence());  
-                            op = teclado.nextInt();
-                           
-                            while (op<2 && op>=1){
-                            teclado.nextLine();
-                                switch (op){
-                                    case 1:{
-                                       System.out.println(radioUvg.getFrequence()); 
-                                         radioUvg.forward();
-                                         break;   
-                                    }
-                                    case 2:{
-                                        System.out.println(radioUvg.getFrequence()); 
-                                         radioUvg.backward();
-                                         break;
-                                    }
-                                         
-                                 }          
+                case 1 -> {
+                    radioUvg.turnOnOff();
+                    System.out.println("Radio encendida");
+
+                    onState: while (true){
+                        System.out.println(menu);
+                        System.out.println("Ingrese la opción que desea: ");
+                        int op = Integer.parseInt(teclado.nextLine());
+                        switch (op){
+                            case 1 -> {
+                                radioUvg.changeFrequence();
+                                System.out.println(radioUvg.getFrequence());
                             }
-                        break;
-                        }
-                        case 3:{
-                            System.out.println("Escriba el numero de boton en el cual lo quiere guardar del 1 al 12");
-                            opcion = teclado.nextInt();
-                            teclado.nextLine();  
-                            radioUvg.saveStation(opcion);
-                            System.out.println("Se guardo la emisora");
-                            break;
-                        }
-                        case 4:{
-                            System.out.println("Escriba el numero de boton en el cual se encuentra su emisora");
-                            opcion = teclado.nextInt();
-                            teclado.nextLine();  
-                            radioUvg.selectFavoriteSation(opcion);
-                            System.out.println("Se ha puesto la emisora"); 
-                        }
-                        case 5:{
-                            
+
+                            case 2 -> {
+                                dial: while (true) {
+                                    System.out.println("presione 1 para avanzar y presione 2 para retroceder y 3 para salir");
+                                    System.out.println("usted esta en la frecuencia:");
+                                    System.out.println(radioUvg.getFrequence());
+                                    System.out.println("y en la estacion:");
+                                    if (radioUvg.getFrequence().equals("AM")){
+                                        System.out.println(radioUvg.getAMActualStation());
+                                    }else{
+                                        System.out.println(radioUvg.getFMActualStation());
+                                    }
+                                    int dial = Integer.parseInt(teclado.nextLine());
+                                    switch (dial) {
+                                        case 1 -> {
+                                            System.out.println(radioUvg.getFrequence());
+                                            radioUvg.forward();
+                                        }
+                                        case 2 -> {
+                                            System.out.println(radioUvg.getFrequence());
+                                            radioUvg.backward();
+                                        }
+                                        case 3 -> {
+                                            break dial;
+                                        }
+                                    }
+                                }
+                            }
+
+                            case 3 -> {
+                                System.out.println("Escriba el numero de boton en el cual lo quiere guardar del 1 al 12");
+                                opcion = teclado.nextInt();
+                                teclado.nextLine();
+                                radioUvg.saveStation(opcion);
+                                System.out.println("Se guardo la emisora");
+                            }
+                            case 4 -> {
+                                System.out.println("Escriba el numero de boton en el cual se encuentra su emisora");
+                                opcion = teclado.nextInt();
+                                teclado.nextLine();
+                                radioUvg.selectFavoriteSation(opcion);
+                                System.out.println("Se ha puesto la emisora");
+                                break;
+                            }
+                            case 5 -> {
+                                break onState;
+                            }
                         }
                     }
-
-
-                    break;
-            }
-        }
-                case 2: {
-                        System.out.println("Gracias por usar radio uvg"); 
-                            break;
-                            }
-              
+                }
+                case 2 -> {
+                    System.out.println("Gracias por usar radio uvg");
+                    System.exit(0);
+                }
             }
         }
     }
